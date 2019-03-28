61e45172992155d17b1b58cbcdfc6bf09e8be486
Ahmed Qarmout <ahmed.qarmout@mail.utoronto.ca>
2019-03-28 19:26:53 -0400

    uploading images from the phone app to a temp server

--- ahmed_test_stuff/index.js
+++ ahmed_test_stuff/index.js
@@ -1,55 +1,34 @@
-var express = require('express');
-var app = express();
-const fileUpload = require('express-fileupload');
-const https = require('https');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const options = {
-   key: fs.readFileSync(`${__dirname}/Keys/private.key`),
-   cert: fs.readFileSync(`${__dirname}/Keys/cert.crt`)
-};
 
-app.use(fileUpload({
-   limits: {
-       fileSize: 50000
-   },
-   safeFileNames: true,
-   preserveExtension: true,
-   abortOnLimit: true
-}));
-app.use(bodyParser.urlencoded({
-   limit: '50mb',
-   extended: true
-}));
+const Express = require('express')
+const multer = require('multer')
+const bodyParser = require('body-parser')
 
-app.use('/src', express.static(`${__dirname}/src`));
+const app = Express()
+app.use(bodyParser.json())
 
-var server = app.listen(9081, function () {
-   var host = server.address().address
-   var port = server.address().port
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './uploads')
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+  },
+})
 
-   console.log("Example app listening at http://%s:%s", host, port);
-});
+const upload = multer({ storage: Storage })
 
-const httpsServer = https.createServer(options, app);
+app.get('/', (req, res) => {
+  res.status(200).send('You can post to /api/upload.')
+})
 
-httpsServer.listen(9082, function () {
-   console.log('https on!');
-});
+app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+  console.log('file', req.files)
+  console.log('body', req.body)
+  res.status(200).json({
+    message: 'success!',
+  })
+})
 
-
-app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/uploadImage', function (req, res) {
-   console.log("got an image");
-   const uploadedFile = req.body.imgBase64.replace(/^data:image\/png;base64,/, "");;
-   fs.writeFile(__dirname + '/imgs/'+req.body.name, uploadedFile, 'base64', function (err) {
-      if (err) {
-         console.log(err);
-         return res.status(200).send(JSON.stringify({'status':'ok'}));
-      }
-      return res.status(200).send("ok");
-  });
-});
+app.listen(3000, () => {
+  console.log('App running on http://localhost:3000')
+})
--- ahmed_test_stuff/phone_app/BubbleGrader/App.js
+++ ahmed_test_stuff/phone_app/BubbleGrader/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Button, KeyboardAvoidingView, Image } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import FileUploader from 'react-native-file-uploader'
 
 export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
+    scanning: true,
+    imgURI: ""
   };
 
   async componentDidMount() {
@@ -19,7 +22,7 @@ export default class CameraExample extends React.Component {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
-    } else {
+    } else if (this.state.scanning === true) {
       return (
         <View style={{ flex: 1 }}>
           <Camera style={{ flex: 1 }} type={this.state.type}
@@ -29,8 +32,86 @@ export default class CameraExample extends React.Component {
           ref={ref => { this.camera = ref; }} 
           onBarCodeScanned={ async () => {
             if (this.camera) {
+              const pictureSizes = await this.camera.getAvailablePictureSizesAsync('4:3');
+
+              // alert(pictureSizes);
+              // {pictureSize: pictureSizes[0], base64: true}
               let photo = await this.camera.takePictureAsync();
               alert(JSON.stringify(photo));
+              this.setState({ scanning: false, imgURI: photo.uri });
+
+              const data = new FormData();
+
+              data.append("photo", {
+                name: 'lol.jpg',
+                type: 'image/jpeg',
+                uri: photo.uri
+              });
+
+              fetch("http://142.1.4.130:3000/api/upload", {
+                method: "POST",
+                body: data
+              });
+
+
+              return;
+
+              const obj = {
+                uploadUrl: 'http://142.1.3.177:9081/ping',
+                method: 'POST', // default 'POST',support 'POST' and 'PUT'
+                headers: {
+                  'Accept': 'application/json',
+                },
+                fields: {
+                    'hello': 'world',
+                },
+                files: [
+                  {
+                    name: 'one', // optional, if none then `filename` is used instead
+                    filename: 'one.w4a', // require, file name
+                    filepath: photo.uri, // require, file absoluete path
+                    filetype: 'image/jpeg', // options, if none, will get mimetype from `filepath` extension
+                  },
+                ]
+              };
+
+              FileUpload.upload(obj, function(err, result) {
+                console.log('upload:', err, result);
+              })
+
+              return;
+
+              let formData = new FormData();
+              formData.append('photouri', {uri: photo.uri, name: 'image.jpg', type: 'image/jpeg'});
+              
+              fetch('http://142.1.3.177:9081/ping', {
+                method: 'POST',
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'multipart/form-data',
+                },
+                body: formData
+              }).then(response => {
+                console.log("image uploaded");
+              }).catch(err => {
+                console.error(err);
+              });
+              
+              return;
+
+              let photoObj = {
+                uri: photo.uri,
+                type: 'image/jpeg',
+                name: 'photo.jpg',
+              };
+              
+              var body = new FormData();
+              body.append('imgBase64', photoObj);
+              body.append('name', 'A beautiful photo!');
+              
+              var xhr = new XMLHttpRequest();
+              xhr.open('POST', 'http://142.1.3.177:9081/uploadImage');
+              xhr.send(body);
             }
           }}>
             <View
@@ -47,9 +128,7 @@ export default class CameraExample extends React.Component {
                 }}
                 onPress={() => {
                   this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
+                    scanning: false
                   });
                 }}>
                 <Text
@@ -61,6 +140,20 @@ export default class CameraExample extends React.Component {
           </Camera>
         </View>
       );
+    }else{
+      return <KeyboardAvoidingView  behavior="padding" enabled>
+        <Image
+          source={{
+            uri: this.state.imgURI
+          }}
+          style={{width: 400, height: 400}}
+        />
+        <Button
+          onPress={() => {this.setState({ scanning: true });}}
+          title="Learn More"
+          color="#841584"
+        />
+      </KeyboardAvoidingView>;
     }
   }
 }
